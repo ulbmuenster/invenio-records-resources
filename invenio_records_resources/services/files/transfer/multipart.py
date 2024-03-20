2a2149165b37521806f5aa5629bd0214ed9bbf64
Mirek Simek <miroslav.simek@gmail.com>
2024-03-20 20:36:05 +0100

    removed multipart - will be in another branch

--- invenio_records_resources/services/files/transfer/multipart.py
+++
@@ -1,8 +0,0 @@
-from invenio_records_resources.services.files.transfer import TransferType
-from invenio_records_resources.services.files.transfer.base import BaseTransfer
-
-
-class MultipartTransfer(BaseTransfer):
-    type = TransferType(type="M", is_serializable=False)
-
-    
